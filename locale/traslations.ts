55124cf50c29ea791f27ad6e7eb68be391bcf494
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-12-14 03:28:59 -0300

    Agregar archivo de traducciones con soporte para español, inglés y portugués

---
+++ locale/traslations.ts
@@ -0,0 +1,260 @@
+export const translations = {
+    es: {
+        navbar: {
+            home: "Inicio",
+            service: "Servicios",
+            contact: "Contacto",
+            singUp: "Regístrate",
+        },
+        hero: {
+            title: "INTEGRAMOS EL MUNDO FISICO AL DIGITAL",
+            description:
+                "Con TailGrids, las empresas y los estudiantes prosperan juntos. Las empresas pueden igualar perfectamente su personal a la demanda cambiante a lo largo del día.",
+            contact: "CONTÁCTENOS",
+            download: "Descargar Brochure",
+            clients: "Algunos de nuestros clientes",
+        },
+        qoute: {
+            text: "El marketing no se trata de las cosas que vendes, sino de las historias que cuentas.",
+            description: "Seth Godin",
+            job: "Empresario y autor estadounidense",
+        },
+        cta: {
+            title: "Quienes somos?",
+            subtitle: "Nos dedicamos a brindar soluciones",
+            description:
+                "Es un hecho establecido desde hace mucho tiempo que un lector se distraerá con el contenido legible de una página cuando mire su diseño. El objetivo de utilizar Lorem Ipsum es que tiene un más o menos.",
+            description2:
+                "Un nombre de dominio es uno de los primeros pasos para establecer su marca. Asegure una imagen de marca consistente con un nombre de dominio que coincida con su negocio.",
+            contact: "Háblanos ahora",
+        },
+        projects: {
+            title: "Nuestro Portafolio",
+            subtitle: "Proyectos recientes",
+            description:
+                "Hay muchas variaciones de pasajes de Lorem Ipsum disponibles, pero la mayoría han sufrido alteraciones de alguna forma.",
+            menu: {
+                all: "Todos los proyectos",
+                design: "Diseños",
+                programming: "Programación",
+                marketing: "Marketing",
+            },
+        },
+        form: {
+            subtitle: "Solucionamos tus problemas",
+            title: "Dejanos tu contacto!",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            li1: {
+                title: "Nuestra ubicación",
+                description: "Calle falsa 123",
+            },
+            li2: {
+                title: "Numero de contacto",
+                description: "123456789",
+            },
+            li3: {
+                title: "Correo electronico",
+                description: "info@yourdomain.com",
+            },
+            name: "Tu nombre",
+            email: "Tu email",
+            phone: "Tu teléfono",
+            message: "Tu mensaje",
+            submit: "Enviar mensaje",
+            submitLoading: "Enviando mensaje...",
+        },
+        footer: {
+            description:
+                "Somos una empresa que se dedica a brindar soluciones digitales a empresas y estudiantes. Nuestro objetivo es integrar el mundo físico al digital.",
+            resources: {
+                title: "Recursos",
+                saas: "Desarrollo de software",
+                product: "Nuestros productos",
+                strategy: "Estrategia de marketing",
+            },
+            follow: {
+                title: "Síguenos en",
+                copy: "Todos los derechos reservados",
+            },
+        },
+        title: "Bienvenido a mi sitio web",
+        description: "Este es un ejemplo de una página con cambio de idioma",
+        home: "Inicio",
+        about: "Acerca de",
+        contact: "Contacto",
+        language: "Idioma",
+    },
+    en: {
+        navbar: {
+            home: "Home",
+            service: "Services",
+            contact: "Contact",
+            singUp: "Sing Up",
+        },
+        hero: {
+            title: "WE INTEGRATE THE PHYSICAL WORLD TO THE DIGITAL",
+            description:
+                "With TailGrids, businesses and students thrive together. Businesses can perfectly match their staffing to changing demand throughout the day.",
+            contact: "CONTACT US",
+            download: "Download Brochure",
+            clients: "Some Of Our Clients",
+        },
+        qoute: {
+            text: "Marketing is not about the things you sell, but the stories you tell.",
+            description: "Seth Godin",
+            job: "American entrepreneur and author",
+        },
+        cta: {
+            title: "Who we are?",
+            subtitle: "We are dedicated to providing solutions",
+            description:
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less.",
+            description2:
+                "A domain name is one of the first steps to establishing your brand. Ensure a consistent brand image with a domain name that matches your business.",
+            contact: "Talk to us now",
+        },
+        projects: {
+            title: "Our Portfolio",
+            subtitle: "Recent Projects",
+            description:
+                "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
+            menu: {
+                all: "All Projects",
+                design: "Designs",
+                programming: "Programming",
+                marketing: "Marketing",
+            },
+        },
+        form: {
+            subtitle: "We solve your problems",
+            title: "Leave us your contact!",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            li1: {
+                title: "Our location",
+                description: "123 Fake Street",
+            },
+            li2: {
+                title: "Contact number",
+                description: "123456789",
+            },
+            li3: {
+                title: "Email",
+                description: "info@yourdomain.com",
+            },
+            name: "Your name",
+            email: "Your email",
+            phone: "Your phone",
+            message: "Your message",
+            submit: "Submit message",
+            submitLoading: "Sending message...",
+        },
+        footer: {
+            description:
+                "We are a company that is dedicated to providing digital solutions to businesses and students. Our goal is to integrate the physical world into the digital world.",
+            resources: {
+                title: "Resources",
+                saas: "Software development",
+                product: "Our products",
+                strategy: "Marketing strategy",
+            },
+            follow: {
+                title: "Follow us on",
+                copy: "All rights reserved",
+            },
+        },
+        title: "Welcome to my website",
+        description: "This is an example of a page with language switching",
+        home: "Home",
+        about: "About",
+        contact: "Contact",
+        language: "Language",
+    },
+    pt: {
+        navbar: {
+            home: "Início",
+            service: "Serviços",
+            contact: "Contato",
+            singUp: "Inscrever-se",
+        },
+        hero: {
+            title: "INTEGRAMOS O MUNDO FÍSICO AO DIGITAL",
+            description:
+                "Com TailGrids, empresas e estudantes prosperam juntos. As empresas podem combinar perfeitamente sua equipe com a demanda variável ao longo do dia.",
+            contact: "CONTATE-NOS",
+            download: "Baixar Brochura",
+            clients: "Alguns de nossos clientes",
+        },
+        qoute: {
+            text: "Marketing não se trata das coisas que você vende, mas das histórias que você conta.",
+            description: "Seth Godin",
+            job: "Empresário e autor americano",
+        },
+        cta: {
+            title: "Quem somos?",
+            subtitle: "Dedicamo-nos a fornecer soluções",
+            description:
+                "É um fato estabelecido há muito tempo que um leitor será distraído pelo conteúdo legível de uma página ao olhar seu layout. O objetivo de usar Lorem Ipsum é que ele tem um mais ou menos.",
+            description2:
+                "Um nome de domínio é um dos primeiros passos para estabelecer sua marca. Garanta uma imagem de marca consistente com um nome de domínio que corresponda ao seu negócio.",
+            contact: "Fale conosco agora",
+        },
+        projects: {
+            title: "Nosso Portfólio",
+            subtitle: "Projetos Recentes",
+            description:
+                "Existem muitas variações de passagens de Lorem Ipsum disponíveis, mas a maioria sofreu alterações de alguma forma.",
+            menu: {
+                all: "Todos os Projetos",
+                design: "Designs",
+                programming: "Programação",
+                marketing: "Marketing",
+            },
+        },
+        form: {
+            subtitle: "Resolvemos seus problemas",
+            title: "Deixe-nos seu contato!",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            li1: {
+                title: "Nossa localização",
+                description: "Rua Falsa 123",
+            },
+            li2: {
+                title: "Número de contato",
+                description: "123456789",
+            },
+            li3: {
+                title: "Email",
+                description: "info@yourdomain.com",
+            },
+            name: "Seu nome",
+            email: "Seu email",
+            phone: "Seu telefone",
+            message: "Sua mensagem",
+            submit: "Enviar mensagem",
+            submitLoading: "Enviando mensagem...",
+        },
+        footer: {
+            description:
+                "Somos uma empresa dedicada a fornecer soluções digitais para empresas e estudantes. Nosso objetivo é integrar o mundo físico ao digital.",
+            resources: {
+                title: "Recursos",
+                saas: "Desenvolvimento de software",
+                product: "Nossos produtos",
+                strategy: "Estratégia de marketing",
+            },
+            follow: {
+                title: "Siga-nos em",
+                copy: "Todos os direitos reservados",
+            },
+        },
+        title: "Bem-vindo ao meu site",
+        description: "Este é um exemplo de uma página com mudança de idioma",
+        home: "Início",
+        about: "Sobre",
+        contact: "Contato",
+        language: "Idioma",
+    },
+};
