bece03eac443b28b8816ab3527f33f7890dce24f
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-12-14 03:24:16 -0300

    Agregar estructura de diseño y componentes para la página de inicio en múltiples idiomas

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Cta from '@/components/interface/cta';
+import Form from '@/components/interface/form';
+import Hero from '@/components/interface/hero';
+import Projects from '@/components/interface/proyects';
+import Quote from '@/components/interface/quote';
+
+export default function Home({ params }: { params: { lang: string } }) {
+  return (
+    <section className='overflow-hidden'>
+      <Hero />
+      <Quote />
+      <Cta />
+      <Projects />
+      <Form />
+    </section>
+  );
+}
